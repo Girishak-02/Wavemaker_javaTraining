26499174c076befc267666ff7b01bd301d966cca
Girishak-02 <girisha.kanamarlapudi@wavemaker.com>
2024-09-02 12:24:00 +0530

    Delete Leave_Managment_Project directory

--- Leave_Managment_Project/src/main/java/com/Wavemaker/LeaveManagment/Service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.Wavemaker.LeaveManagment.Service;
-
-public interface LoginService {
-    boolean validateLogin(String email, String password) throws ClassNotFoundException;
-    int getLoginId(String email) throws ClassNotFoundException;
-}
--- Leave_Managment_Project/src/main/java/com/Wavemaker/LeaveManagment/repository/LoginRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Wavemaker.LeaveManagment.repository;
-
-import com.Wavemaker.LeaveManagment.model.Login;
-
-public interface LoginRepository {
-    Login getLoginByEmail(String email) throws ClassNotFoundException;
-}
